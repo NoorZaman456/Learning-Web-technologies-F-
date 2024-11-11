3ed43121848fcc260af8232a0eee38d5931e4d86
NoorZaman456 <noorcnr60@gmail.com>
2024-11-11 16:33:59 +0600

    Add files via upload
    Lab 03

---
+++ lab3/task1.php
@@ -0,0 +1,9 @@
+<?php
+$length = 5; 
+$width = 3;  
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+echo "For a rectangle with length = $length and width = $width:<br>";
+echo "Area = $area square units<br>";
+echo "Perimeter = $perimeter units<br>";
+?>
---
+++ lab3/task7.php
@@ -0,0 +1,32 @@
+<?php
+echo "<table border='1' cellpadding='10' cellspacing='0'>";
+echo "<tr>";
+echo "<td>";
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "* ";
+    }
+    echo "<br>";
+}
+echo "</td>";
+echo "<td>";
+for ($i = 3; $i >= 1; $i--) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo $j . " ";
+    }
+    echo "<br>";
+}
+echo "</td>";
+echo "<td>";
+$letter = 'A';
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo $letter . " ";
+        $letter++;
+    }
+    echo "<br>";
+}
+echo "</td>";
+echo "</tr>";
+echo "</table>";
+?>
