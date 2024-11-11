623cce0cf79e1966f1b1e37f250e7e1af69ec95a
NoorZaman456 <noorcnr60@gmail.com>
2024-11-11 16:21:41 +0600

    Delete task1.php

--- task1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$length = 5; 
-$width = 3;  
-$area = $length * $width;
-$perimeter = 2 * ($length + $width);
-echo "For a rectangle with length = $length and width = $width:<br>";
-echo "Area = $area square units<br>";
-echo "Perimeter = $perimeter units<br>";
-?>
